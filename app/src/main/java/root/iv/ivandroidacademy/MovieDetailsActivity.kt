ea7770437299ff115ba561063fbd90c71e5860db
Igor Smirnov <igor.smirnov@deltasolutions.ru>
2020-12-19 16:55:09 +0300

    Скрываю ActionBar во второй активити

--- app/src/main/java/root/iv/ivandroidacademy/MovieDetailsActivity.kt
+++ app/src/main/java/root/iv/ivandroidacademy/MovieDetailsActivity.kt
@@ -7,5 +7,7 @@ class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
+        supportActionBar?.hide()
     }
 }
