aaca74029424dde68a5636c99053d1d715558bfe
Igor Smirnov <igor.smirnov@deltasolutions.ru>
2020-12-19 16:52:32 +0300

    Добавил картинку и градиент

--- app/src/main/java/root/iv/ivandroidacademy/MainActivity.kt
+++ app/src/main/java/root/iv/ivandroidacademy/MainActivity.kt
@@ -1,11 +1,19 @@
 package root.iv.ivandroidacademy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<RelativeLayout>(R.id.mainBackground)
+            .setOnClickListener {
+                val intent = Intent(this, MovieDetailsActivity::class.java)
+                startActivity(intent)
+            }
     }
 }
